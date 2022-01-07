3387d6cc0feb8c11e6ad5cfb717ffe9d898943e8
lisethmdc <lismc1992@gmail.com>
2022-01-07 20:54:46 +0100

    Iteration#5

--- Iteration5.js
+++ Iteration5.js
@@ -48,10 +48,3 @@ let streamOld = streamers3.filter(streamer => {
 console.log(streamOld)
 
 
-//5.6
-const streamers4 = [
-	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
-	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
-	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
-	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
-]
